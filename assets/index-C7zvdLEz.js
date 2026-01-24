31551766faf40dfc60bbfa7618c480a7976cb0ab
Poom <Poom@bitegginc.com>
2026-01-24 23:15:21 +0700

    Updates

--- assets/index-C7zvdLEz.js
+++ assets/index-C7zvdLEz.js
@@ -1,4 +1,4 @@
-import{d as n,al as m,am as x,j as e,ap as h,an as f}from"./index-BKkjUTJi.js";import{F as g}from"./index-BhZoNjmE.js";const j=n("section")`
+import{d as n,al as m,am as x,j as e,ap as h,an as f}from"./index-CHv2Z41O.js";import{F as g}from"./index-B-UEMVgl.js";const j=n("section")`
   position: relative;
   padding: 10rem 0 8rem;
 
